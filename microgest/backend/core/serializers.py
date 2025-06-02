4e23e200d8db0590c5635caf0c37c9e74f326fc1
Solino8 <111749902+Solino8@users.noreply.github.com>
2025-06-02 10:37:40 -0300

    Add files via upload

---
+++ microgest/backend/core/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Produto
+from .models import Venda
+
+class ProdutoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Produto
+        fields = '__all__'
+
+class VendaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venda
+        fields = '__all__'
